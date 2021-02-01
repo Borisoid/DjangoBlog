0a3f6f1c1333329ef24916b09cd95f6528baf881
Borisoid <boris.lazarev90@mail.ru>
2021-02-01 23:07:49 +0300

    add views and urlpatterns

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    raise NotImplementedError
+
+def concrete_post(request, post_id: int):
+    raise NotImplementedError
+
+def add_post(request):
+    raise NotImplementedError

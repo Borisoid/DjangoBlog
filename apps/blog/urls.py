fc64509fa28cb062340c23132cba2d93aedd39d7
Borisoid <boris.lazarev90@mail.ru>
2021-02-02 17:55:09 +0300

    post deletion

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,10 +4,13 @@ from .views import (
    post_list,
    concrete_post,
    add_post,
+   delete_post,
 )
 
 urlpatterns = [
     path('add/', add_post),
-    path('<int:post_id>/', concrete_post),
+    path('post/', concrete_post, name='concrete_post'),
+    path('post/edit/', concrete_post, name='edit_post'),
+    path('post/delete/', delete_post, name='delete_post'),
     path('', post_list),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
 
 from .models import(
     Post,
 )
 
+
+
 def post_list(request):
     raise NotImplementedError
 
-def concrete_post(request, post_id: int):
-    post = Post.objects.filter(id=post_id).first()
-
+def concrete_post(request):
+    
     if request.method == 'GET':
-        return render(request, 'concrete-post.html', {'post': post})
+        post_id = request.GET.get('id')
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post:
+                return render(request, 'concrete-post.html', 
+                    {'post': post,}
+                )
+        
+        raise Http404
+
+def delete_post(request):
+    if request.method == 'GET':
+        post_id = request.GET.get('id')
+        if post_id:
+            Post.objects.filter(id=post_id).delete()
+            
+            return HttpResponse('Deleted')
+
+    raise Http404
 
 def add_post(request):
     raise NotImplementedError

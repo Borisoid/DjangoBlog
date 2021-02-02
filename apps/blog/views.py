b194de4f04997d92ebcf7e004a9bc0545a8b22f4
Borisoid <boris.lazarev90@mail.ru>
2021-02-02 16:09:25 +0300

    GET concrete_post

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from .models import(
+    Post,
+)
+
 def post_list(request):
     raise NotImplementedError
 
 def concrete_post(request, post_id: int):
-    raise NotImplementedError
+    post = Post.objects.filter(id=post_id).first()
+
+    if request.method == 'GET':
+        return render(request, 'concrete-post.html', {'post': post})
 
 def add_post(request):
     raise NotImplementedError
